39aab109efb2f1105e8460ad8e0836554cdd49fb
tsukiroku <tsukiroku@yahoo.com>
2022-06-18 12:05:26 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -11,13 +11,13 @@ ROOT_DIR="./"
 function make {
     rm -rf $ROOT_DIR/*
     rm -rf .gitignore
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/$RAW/package.json" >>$ROOT_DIR/package.json
+    curl -sS "https://recalculated.github.io/typescript-project/$RAW/package.json" >>$ROOT_DIR/package.json
+    curl -sS "https://recalculated.github.io/typescript-project/default/LICENSE" >>$ROOT_DIR/LICENSE
+    curl -sS "https://recalculated.github.io/typescript-project/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
     curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/.gitignore" >>$ROOT_DIR/.gitignore
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/LICENSE" >>$ROOT_DIR/LICENSE
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
     touch index.ts
     mkdir src
-    echo "template has been created."
+    echo "template has been created. ($RAW)"
 }
 
 echo "All files/directories will be deleted."
@@ -35,6 +35,6 @@ select yn in "Y" "N"; do
         break
         ;;
 
-    *) echo "Unknown option '$REPLY'";;
+    *) echo "Unknown option '$REPLY'" ;;
     esac
 done
