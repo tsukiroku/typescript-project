221696fee5fda1b85ff8bb41d9ecb839d87ba412
tsukiroku <tsukiroku@yahoo.com>
2022-06-18 11:56:26 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -11,16 +11,18 @@ ROOT_DIR="./"
 function make {
     rm -rf $ROOT_DIR/*
     rm -rf .gitignore
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/$RAW/.gitignore" >>$ROOT_DIR/.gitignore
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/$RAW/LICENSE" >>$ROOT_DIR/LICENSE
     curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/$RAW/package.json" >>$ROOT_DIR/package.json
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/$RAW/tsconfig.json" >>$ROOT_DIR/tsconfig.json
+    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/.gitignore" >>$ROOT_DIR/.gitignore
+    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/LICENSE" >>$ROOT_DIR/LICENSE
+    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
     touch index.ts
     mkdir src
+    echo "template has been created."
 }
 
-echo "⚠️  All files/directories will be deleted."
+echo "All files/directories will be deleted."
 
+PS3="Select [1/2]: "
 select yn in "Y" "N"; do
     case $yn in
     Y)
@@ -32,5 +34,7 @@ select yn in "Y" "N"; do
         rm -rf make.sh
         break
         ;;
+
+    *) echo "Unknown option '$REPLY'";;
     esac
 done
