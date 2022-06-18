69fa942a5812532bd929a1ee98f796a3f59dcc55
tsukiroku <tsukiroku@yahoo.com>
2022-06-18 12:09:50 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -11,12 +11,15 @@ ROOT_DIR="./"
 function make {
     rm -rf $ROOT_DIR/*
     rm -rf .gitignore
+    touch index.ts
+    mkdir src
+    mkdir .github && mkdir .github/workflows
     curl -sS "https://recalculated.github.io/typescript-project/$RAW/package.json" >>$ROOT_DIR/package.json
     curl -sS "https://recalculated.github.io/typescript-project/default/LICENSE" >>$ROOT_DIR/LICENSE
     curl -sS "https://recalculated.github.io/typescript-project/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
+    curl -sS "https://recalculated.github.io/typescript-project/default/.json" >>$ROOT_DIR/tsconfig.json
+    curl -sS "https://recalculated.github.io/typescript-project/default/npm_publish.yml" >>$ROOT_DIR/.github/workflows/npm_publish.yml
     curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/.gitignore" >>$ROOT_DIR/.gitignore
-    touch index.ts
-    mkdir src
     echo "template has been created. ($RAW)"
 }
 
