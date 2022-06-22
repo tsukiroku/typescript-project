a14dbccb475739ca4c0545e4820d1aa67a6fd4e5
Kim Jun Young <tsukiroku@yahoo.com>
2022-06-22 17:17:50 +0900

    Update make.sh

--- make.sh
+++ make.sh
@@ -14,14 +14,14 @@ function make {
     rm -rf .github
     touch index.ts
     mkdir src
-    curl -sS "https://recalculated.github.io/typescript-project/$RAW/package.json" >>$ROOT_DIR/package.json
-    curl -sS "https://recalculated.github.io/typescript-project/default/LICENSE" >>$ROOT_DIR/LICENSE
-    curl -sS "https://recalculated.github.io/typescript-project/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
-    curl -sS "https://raw.githubusercontent.com/recalculated/typescript-project/main/default/.gitignore" >>$ROOT_DIR/.gitignore
+    curl -sS "https://tsukiroku.github.io/typescript-project/$RAW/package.json" >>$ROOT_DIR/package.json
+    curl -sS "https://tsukiroku.github.io/typescript-project/default/LICENSE" >>$ROOT_DIR/LICENSE
+    curl -sS "https://tsukiroku.github.io/typescript-project/default/tsconfig.json" >>$ROOT_DIR/tsconfig.json
+    curl -sS "https://raw.githubusercontent.com/tsukiroku/typescript-project/main/default/.gitignore" >>$ROOT_DIR/.gitignore
     
     if [ "$RAW" = "npm-package" ]; then
         mkdir .github && mkdir .github/workflows
-        curl -sS "https://recalculated.github.io/typescript-project/npm-package/npm_publish.yml" >>$ROOT_DIR/.github/workflows/npm_publish.yml
+        curl -sS "https://tsukiroku.github.io/typescript-project/npm-package/npm_publish.yml" >>$ROOT_DIR/.github/workflows/npm_publish.yml
         echo "template has been created. ($RAW)"
     fi
 }
